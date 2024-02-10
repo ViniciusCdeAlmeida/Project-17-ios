c87e34f2a5e3f4a491c2c1c74e8cb54479c36558
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-10 18:37:40 -0300

    Initial Commit

---
+++ businessCard/businessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessCardApp.swift
+//  businessCard
+//
+//  Created by Apple Multiplier on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct businessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
