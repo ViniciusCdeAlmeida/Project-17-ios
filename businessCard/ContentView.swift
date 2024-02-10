392d3fca13aceab7529285c85081ea081a9d835e
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-10 19:48:38 -0300

    feat: business card

--- businessCard/ContentView.swift
+++ businessCard/ContentView.swift
@@ -9,18 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("foto1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.accentColor)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+
+                Text("Vinícius Corrêa")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "Phone Number")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    let text: String
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(height: 30.0)
+                .foregroundColor(.white)
+            HStack {
+                Image(systemName: "phone.fill")
+                Text(text)
+                    .foregroundColor(.black)
+                    .font(.system(size: 25))
+            }
+        }
     }
 }
